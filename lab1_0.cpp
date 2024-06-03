ffb256e5a5ffa8ccf5373404b5557dfb18e5a313
ququha <gosh.shamin909@gmail.com>
2024-06-03 12:32:29 +0700

    q

--- lab1_0.cpp
+++ lab1_0.cpp
@@ -45,12 +45,14 @@ int main(){
     setlocale(LC_ALL, "Russian");
     int a, b, h;
     string figura, x;
+
     cout << "Выберите фигуру (p-прямоугольник, t-трапеция) : ";
     cin >> figura;
     if (figura=="p"){
     for (int i=0, n=1; i<n; i++, n++){
         cout <<"Введите стороны прямоугольника: ";
         cin >> a >> b;
+
         pryam(a, b);
         cout << "Хотите продолжить? (y/n) ";
         cin >> x;
